f1c7f5e391a96df15e105da59732109aa2e29ad1
kwangminChoi <rhkd4560@naver.com>
2019-08-07 23:21:50 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import {withStyles} from '@material-ui/core/styles'
+import { async } from 'rxjs/internal/scheduler/async';
 
 
 const styles = theme => ({
@@ -19,36 +20,27 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-
-const customers= [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job' : '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '최광민',
-  'birthday': '950914',
-  'gender': '남자',
-  'job' : '대학생'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '이순신',
-  'birthday': '951234',
-  'gender': '남자',
-  'job' : '디자이너'
-}
-]
 
 class App extends Component{
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const{ classes} = this.props;
     return (
@@ -66,7 +58,7 @@ class App extends Component{
           </TableHead>
           <TableBody>
           {
-            customers.map(c => {
+            this.state.customers ? this.state.customers.map(c => {
               return (
               <Customer
                 key = {c.id}
@@ -78,8 +70,7 @@ class App extends Component{
                 job = {c.job}
               />          
               );
-            })
-          }
+            }) : "" }
           </TableBody>
         </Table>
       </Paper>
